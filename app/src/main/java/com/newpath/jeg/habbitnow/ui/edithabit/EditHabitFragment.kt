e218569fe1a43ac404603b76471fc0ff885cfb1c
Javi G <javglex@yahoo.com>
2020-10-03 00:09:58 -0700

    Toolbar changes depending on fragment visible. Added a checkmark for the EditHabitFragment.kt. Checkmark finishes editing the Habit Item

--- app/src/main/java/com/newpath/jeg/habbitnow/ui/edithabit/EditHabitFragment.kt
+++ app/src/main/java/com/newpath/jeg/habbitnow/ui/edithabit/EditHabitFragment.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.newpath.jeg.habbitnow.R
 import com.newpath.jeg.habbitnow.databinding.FragmentEditHabitBinding
-import com.newpath.jeg.habbitnow.models.MyHabit
+
 
 /**
  * Used to create new habits or edit existing ones
@@ -41,27 +39,28 @@ class EditHabitFragment : Fragment() {
 
         // Get a reference to the binding object and inflate the fragment views.
         val binding: FragmentEditHabitBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_edit_habit, container, false)
+            inflater, R.layout.fragment_edit_habit, container, false
+        )
 
         editHabitViewModel = ViewModelProvider(this).get(EditHabitViewModel::class.java)
 
         if (habitId!=null){
             editHabitViewModel.mHabitId = habitId
-            Log.d(TAG,"recognized id, editing habit..")
+            Log.d(TAG, "recognized id, editing habit..")
             if (habitName != null) {
                 editHabitViewModel.mHabitName = habitName
             }
-        } else Log.d(TAG,"seems like it's a new habit, creating..")
+        } else Log.d(TAG, "seems like it's a new habit, creating..")
 
         binding.lifecycleOwner = this
         binding.editHabitViewModel = editHabitViewModel
         binding.etHabitName.setText(editHabitViewModel.mHabitName)
-        binding.etHabitName.addTextChangedListener(object: TextWatcher{
+        binding.etHabitName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                if (p0==null || p0.isEmpty())
+                if (p0 == null || p0.isEmpty())
                     return
                 editHabitViewModel.mHabitName = p0.toString()
             }
@@ -76,9 +75,38 @@ class EditHabitFragment : Fragment() {
             editHabitViewModel.submitHabit()
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_done, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
 
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        menu.findItem(R.id.action_settings)?.isVisible = false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        when(id) {
+            R.id.action_done -> {
+                Log.d(TAG, "checkmark hit!")
+                findNavController().popBackStack()
+                editHabitViewModel.submitHabit()
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+        return true;
+
+    }
 
 }
